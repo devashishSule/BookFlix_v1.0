64f3c2e01d26fa44ec678958ad9758b6183515db
devashishSule <devashish.sule@gmail.com>
2022-11-20 00:44:20 +0530

    **Login page**

--- app.py
+++ app.py
@@ -5,22 +5,34 @@ app = Flask(__name__)
 
 client = MongoClient("mongodb+srv://Devashish:Devashish2002@cluster0.wurmd5z.mongodb.net/?retryWrites=true&w=majority")
 db = client.BookFlix_db
-register = db.register
+register_dets = db.register_dets
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/')
 def index():
+    return render_template('index.html')
+     
+
+@app.route('/register.html', methods=('GET', 'POST'))
+def register():
     if request.method=='POST':
         firstName = request.form['content1']
         lastName = request.form['content2']
         email = request.form['email']
-        register.insert_one({
+        password = request.form['password']
+        register_dets.insert_one({
             'first name':firstName, 
             'last name':lastName, 
-            'email':email
+            'email':email,
+            'password':password
         })
         return render_template('verified.html')
-    all_info = register.find()
-    return render_template('index.html', register=all_info)
+    all_info = register_dets.find()
+    return render_template('register.html', register_dets=all_info)
+
+@app.route('/login.html')
+def login():
+    
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
