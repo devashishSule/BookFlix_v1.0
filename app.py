46387af509cebc128fbb9151c24cde717f2049dd
devashishSule <devashish.sule@gmail.com>
2022-11-24 16:35:09 +0530

    **Commit init**

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
+# from flask_mail import Mail,Message
 
 app = Flask(__name__)
+# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'sule.devashish@gmail.com'
+# app.config['MAIL_PASSWORD'] = 'pkjdoanccxiflxsq'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
+# mail = Mail(app)
 
 client = MongoClient("mongodb+srv://Devashish:Devashish2002@cluster0.wurmd5z.mongodb.net/?retryWrites=true&w=majority")
 db = client.BookFlix_db
-register_dets = db.register_dets
+user_info = db.register_dets
 
 @app.route('/')
 def index():
@@ -18,20 +26,42 @@ def register():
         firstName = request.form['content1']
         lastName = request.form['content2']
         email = request.form['email']
-        password = request.form['password']
-        register_dets.insert_one({
-            'first name':firstName, 
-            'last name':lastName, 
-            'email':email,
-            'password':password
-        })
-        return render_template('verified.html')
-    all_info = register_dets.find()
-    return render_template('register.html', register_dets=all_info)
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+        security_question = request.form['security']
+        if password1==password2: 
+            user_info.insert_one({
+                'first name':firstName, 
+                'last name':lastName, 
+                'email':email,
+                'password':password1,
+                'security_question':security_question
+            })
+            # msg = Message(
+            #     "BookFlix - Acknowledgement letter",
+            #     sender='joemama@hotline.com',
+            #     recipients=[email]
+            # )
+            # msg.body = "Thank You for creating an account in BookFlix."
+            # mail.send(msg)
+            return render_template('verified.html')
+        else:
+            return render_template('register.html')
+            
+    all_info = user_info.find()
+    return render_template('register.html', user_info=all_info)
 
 @app.route('/login.html')
 def login():
-    
+    if request.method=="GET":
+        email = request.form['email']
+        password = request.form['password']
+        verify_email = request.get('email')
+        verify_pass = request.get('password')
+        if email == verify_email and password == verify_pass:
+            return "Welcome %s"%email
+        else:
+            return "Data not found..."
     return render_template('login.html')
 
 if __name__ == "__main__":
